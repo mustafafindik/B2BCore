01ce78eee57db9c30a993fdc49abc9c1dbf47bb6
mustafa.findik <mustafa.findik@cenasis.com.tr>
2019-07-01 15:52:03 +0300

    Some Companent

---
+++ EkominiB2B.WebUI/Components/Shortcuts.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EkominiB2B.WebUI.Components
+{
+    public class Shortcuts:ViewComponent
+    {
+   
+        public IViewComponentResult Invoke()
+        {         
+            return View();
+        }
+    }
+
+
+}
---
+++ EkominiB2B.WebUI/Components/UserProfileLayout.cs
@@ -0,0 +1,25 @@
+﻿using EkominiB2B.DataAccess.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EkominiB2B.WebUI.Components
+{
+    public class UserProfileLayout : ViewComponent
+    {
+        private readonly IUserRepository _userRepository;
+        public UserProfileLayout(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var user = _userRepository.GetByName(User.Identity.Name);
+            return View(user);
+        }
+
+    }
+}
